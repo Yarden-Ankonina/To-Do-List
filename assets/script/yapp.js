e604dd7bb2dacd1ce5fb0e29bab62bd939c64313
Yarden Ankonina <yardi1994@gmail.com>
2020-03-26 04:26:47 +0200

    js update

---
+++ assets/script/yapp.js
@@ -0,0 +1,17 @@
+let vanilla = document.querySelectorAll('.vanilla')[0].children;
+let framework = document.querySelectorAll('.framework')[0].children;
+let css = document.querySelectorAll('.css')[0].children;
+
+//create array
+let column = [vanilla,framework,css];
+
+column.forEach(element=>{
+    for(let i = 0;i<element.length;i++){
+        console.log(element[i]);
+        element[i].addEventListener('click',function(){
+            element[i].classList.toggle("pressed");
+        });
+    }
+   
+});
+
