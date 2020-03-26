626663159990b50746c5e54826370c665e0b7460
Yarden Ankonina <yardi1994@gmail.com>
2020-03-26 04:31:49 +0200

    4:31

--- assets/script/list.js
+++ assets/script/list.js
@@ -1,3 +1,4 @@
+
 let vanilla = document.querySelectorAll('.vanilla')[0].children;
 let framework = document.querySelectorAll('.framework')[0].children;
 let css = document.querySelectorAll('.css')[0].children;
@@ -5,6 +6,7 @@ let css = document.querySelectorAll('.css')[0].children;
 //create array
 let column = [vanilla,framework,css];
 
+// line through and gray - go through each item and if click toggle class
 column.forEach(element=>{
     for(let i = 0;i<element.length;i++){
         console.log(element[i]);
