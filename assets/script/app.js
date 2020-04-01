79147bdbaf01a238a7d4a9853a0238eb31c34c3a
Marcos Molina <53741892+marcosmol204@users.noreply.github.com>
2020-04-02 01:37:04 +0300

    NEW FUNCTIONAL

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,98 +1,127 @@
-/* HTML ELEMENTS */
-const FORM = document.getElementById('form');
-const VANILLACOLUMN = document.getElementById('vanilla');
-const FRAMEWORKCOLUMN = document.getElementById('framework');
-const OTHERCOLUMN = document.getElementById('other');
-const ERRORELEMNT = document.getElementById('error');
-const USERINPUTS = document.querySelectorAll('input');
-const URLINPUT = document.getElementById('url');
-let MY_STORAGE = window.localStorage;
-/* OBJECT OF HTML ELEMNTS */
-const COLUMNS = {
-    'vanilla': VANILLACOLUMN,
-    'framework': FRAMEWORKCOLUMN,
-    'other': OTHERCOLUMN
+class Task {
+  constructor(title, priority,urgency) {
+    this.title = title;
+    this.priority = priority;
+    this.urgency = urgency;
+    this.createdTime = Date.parse(new Date());
+  }
 }
 
+class UI {
 
-/* FORM EVENT*/
-FORM.addEventListener('submit', (e) => {
-    e.preventDefault();
-    submitHandler()
-})
-/* CLEAN THE URL INPUT AFTER AN ERROR */
-URLINPUT.addEventListener('input', () => {
-    ERRORELEMNT.innerHTML = '';
-})
+  static displayTasks() {
+    const tasks = Store.getTasks();
 
-/* addNew() receives as parameter column destination, title and url
-creates and HTML element and insert to the column
-*/
-function addNew(column, title, url) {
+    tasks.forEach((task) => UI.addTaskToList(task));
+  }
+
+  static addTaskToList(task) {
+    const list = document.querySelector('.todo-list'); 
+
     const li = document.createElement('li');
-    li.draggable= true;
-    li.classList = 'draggable';
-    li.addEventListener('dragstart', () => {
-        li.classList.add('dragging')
-      })
-      li.addEventListener('dragend', () => {
-        li.classList.remove('dragging')
-        })
+
+    li.innerHTML = `<a href="#">${task.title}</a>
+    <i class="fas fa-trash-alt"></i>
+    <i class="far fa-edit"></i>`;
+
+    li.firstElementChild.nextElementSibling.addEventListener('click', (event) => {
+      UI.removeTaskFromList(event); })
+    // edit feature must be added
+    list.appendChild(li);
+  }
+  
+  static removeTaskFromList(event) {
+    const title = event.target.previousElementSibling.textContent;
+    const element = event.target.parentNode;
+    Store.removeTask(title);
+    element.remove();
+  }
+
+  // static showAlert(message, className) {
+  //   // DISCUSSE LOGIC WITH YARDEN
+  //   // Vanish in 1.5 seconds
+  //   setTimeout(() => document.querySelector('.alert').remove(), 1500);
+  // }
+
+  static clearFields() {
+    document.querySelector('#title-input').value = '';
+    document.querySelector('#priority-input').value = '1';
+    document.querySelector('#urgency-input').value = '1';
+  }
+
+  static updateTask() {
+    const tasks = Store.getTasks();
+
+    tasks.sort(UI.compare);
+    Store.updateTasks(tasks);
+
+    const elements = Array.from(document.getElementsByClassName('todo-list')[0].children);
+    for ( let i= 1; i< elements.length; i++) {
+      elements[i].remove();
+    }
+    UI.displayTasks();
+  }
+
+
+  static compare(a,b) {
+    if (a.priority === b.priority) {
+      return 0;
+    } else {
+    return a.parameter > b.parameter ? 1: -1;
+  }
+}
+}
+
+class Store {
+  static getTasks() {
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return tasks;
+  }
+
+  static addTask(task) {
+    const tasks = Store.getTasks();
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static removeTask(title) {
+    const tasks = Store.getTasks();
+
+    tasks.forEach((task, index) => {
+      if(task.title === title) {
+        tasks.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static updateTasks(tasks){
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', UI.displayTasks);
+
+document.querySelector('.main-content-todoForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#title-input').value;
+  const priority = document.querySelector('#priority-input').value;
+  const urgency = document.querySelector('#urgency-input').value;
     
-    const span = document.createElement('span');
-    span.classList = 'trash';
+  const task = new Task(title,priority,urgency);
+  UI.addTaskToList(task);
+  Store.addTask(task);
+  UI.clearFields();
+});
 
-    const a = document.createElement('a');
-    a.href = url;
-    a.target = '_blank';
-    a.innerHTML = title;
-
-    span.appendChild(a);
-    // span.addEventListener('click',(e)=>{deleteHandler(e)}); //Adding event handler
-
-    li.appendChild(span);
-    li.appendChild(a);
-
-    column.appendChild(li);
-
-}
-
-/*AddHandler is a eventHandler that process the user inputs 
-and call to AddNew with the processed inputs*/
-function submitHandler() {
-    const title = USERINPUTS[0].value;
-    const url = USERINPUTS[1].value;
-    const columnInput = document.querySelector('select').value;
-    const userInputs = document.querySelectorAll('input');
-    if (isValidURl(url)) {
-        // pushToArray(columnInput,title,url)
-        addNew(COLUMNS[columnInput], title, url);
-        clearInput(userInputs);
-    } else if (!url) {
-        // pushToArray(columnInput,title,'#')
-        addNew(COLUMNS[columnInput], title, '#');
-        clearInput(userInputs);
-    }
-    else {
-        ERRORELEMNT.innerHTML = 'Invalid Url. Please follow the format => https://google.com';
-    }
-}
-
-//Takes InputArrays and clean them in the DOM
-function clearInput() {
-    USERINPUTS.forEach(input =>{ input.value = '';})
-}
-
-function isValidURl(url) {
-    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-    return pattern.test(url);
-}
-
-function addOnLoad(column,title,url) {
-    addNew(COLUMNS[column],title,url);
-}
+document.querySelector('#a').addEventListener('click',(e)=>
+{
+  UI.updateTask();})
