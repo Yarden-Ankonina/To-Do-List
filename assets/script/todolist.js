9061633174d043353be587d5434962056eeb3503
Marcos Molina <53741892+marcosmol204@users.noreply.github.com>
2020-04-09 03:03:49 +0300

    asd
    zxc

--- assets/script/todolist.js
+++ assets/script/todolist.js
@@ -10,42 +10,49 @@ const CLOSE_POPUP = document.querySelector("#popup-exit");
 const SUBMIT_POPUP = document.querySelector(".edit-submit-update")
 const MAIN_FLEX = document.querySelector(".main-flex");
 
-function editTaskHandler(event) {
-    let id = event.target.parentElement.id
-    EDIT_POPUP.style.visibility = "visible";
-    MAIN_FLEX.classList.add("blurBackground");
-
-    document.querySelector('#popup-title-input').value =
-        event.target.previousElementSibling.textContent;
-    document.querySelector('#popup-priority-select').value =
-        event.target.parentElement.classList.value.slice(-1);
-
-    const promise = new Promise((resolve) => {
+function editPromise(event) {
+    const id = event.target.parentElement.id;
+    return new Promise((resolve) => {
         SUBMIT_POPUP.addEventListener('click', (event) => {
             event.preventDefault();
             const newTitle = document.querySelector('#popup-title-input').value;
             const newPriority = document.querySelector('#popup-priority-select').value;
             resolve([newTitle, newPriority, id]);
         });
-        CLOSE_POPUP.addEventListener('click', (event) => {
-            console.log(event)
+        CLOSE_POPUP.addEventListener('click', () => {
             resolve("");
         });
     });
+}
+function showEditPopup() {
+    EDIT_POPUP.style.visibility = "visible";
+    MAIN_FLEX.classList.add("blurBackground");
+}
+function hideEditPopup() {
+    EDIT_POPUP.style.visibility = "hidden";
+    MAIN_FLEX.classList.remove("blurBackground");
+}
+function fillEditPopupInputs() {
+    document.querySelector('#popup-title-input').value =
+        event.target.previousElementSibling.textContent;
+    document.querySelector('#popup-priority-select').value =
+        event.target.parentElement.className.slice(-1);
+}
+function editTaskHandler(event) {
+    showEditPopup();
+    fillEditPopupInputs(event);
 
-    promise.then(response => {
+
+    editPromise(event).then(response => {
         if (!response) {
-            EDIT_POPUP.style.visibility = "hidden";
-            MAIN_FLEX.classList.remove("blurBackground");
+            hideEditPopup();
         }
         else {
-            console.log(response)
             const newTitle = response[0];
             const newPriority = response[1];
             const id = response[2];
             UI.updateTask(id, newTitle, newPriority);
-            EDIT_POPUP.style.visibility = "hidden";
-            MAIN_FLEX.classList.remove("blurBackground");
+            hideEditPopup();
         }
     });
     //
