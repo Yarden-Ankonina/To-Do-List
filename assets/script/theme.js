a5389b2d157cbddd36681c5fe19c475ab05fcc02
Yarden Ankonina <yardi1994@gmail.com>
2020-04-18 04:24:20 +0300

    theme and light dark

---
+++ assets/script/theme.js
@@ -0,0 +1,22 @@
+const THEME = document.querySelector(".fa-tint");
+const LIGHT = document.querySelector(".fa-lightbulb");
+let htmlTheme = document.querySelector("html");
+let hueCounter = 0;
+
+
+THEME.addEventListener('click',function(){
+    hueCounter += 30;
+    htmlTheme.style.filter = "hue-rotate(" + hueCounter +"deg)";
+});
+
+LIGHT.addEventListener('click',function(){
+
+    if(htmlTheme.hasAttribute('data-theme')){
+        htmlTheme.removeAttribute('data-theme', 'light');
+    }
+    else{
+        htmlTheme.setAttribute('data-theme', 'light');
+    }
+    hueCounter = 0;
+    htmlTheme.style.filter = "hue-rotate(" + hueCounter +"deg)";
+});
