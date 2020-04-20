372eaf48f5230a19ec4d2558f766d82a5215c8fc
Marcos Molina <53741892+marcosmol204@users.noreply.github.com>
2020-04-20 13:44:55 +0300

    sad

--- assets/script/events.js
+++ assets/script/events.js
@@ -1,38 +1,29 @@
 import {
-    toogleMainHeaderDisplay, toogleAddForm,
-    toogleSearchBarDisplay, addOptiontoSelect,
-    hideLandingPopUp, removeAllTasks,
-    addTask
+    displayToogle, addOptiontoSelect,
+    removeAllTasks, addTask
 } from './DOM.js'
-import { sortTasksHandler, searchHandler, createTaskHandler, swapHandler } from './handlers.js'
+
+import {
+    FORM_NEW_TODO, INPUT_SEARCH_TEXT, SORT_BUTTON,
+    MENU_BUTTON, MAIN_HEADER_MENU, SEARCH_BUTTON,
+    FORM_SEARCH, ADD_FORM_BUTTON, SELECT_CURRENT_LIST,
+    LANDING_FORM, POPUP_LANDING_TEXT_INPUT, DRAGGABLE_ZONE
+
+} from './DOM.js'
+
+import {
+    sortTasksHandler, searchHandler,
+    createTaskHandler, swapHandler
+} from './handlers.js'
+
 import { setTasksArray, getTasksArray } from './storage.js'
 
 export function addEventsListenerHandler() {
-    const LANDING_FORM = document.querySelector('.popup-first-form');
-    const POPUP_LANDING_TEXT_INPUT = document.querySelector('#first-text-input');
-
-
-    const SEARCH_INPUT = document.querySelector('#search-input');
-    const ADD_FORM_BUTTON = document.querySelector(".fa-plus");
-    const ADD_FORM = document.querySelector(".main-content-forms-todoForm");
-    const SORT_BUTTON = document.querySelector("#navbar-sort-button");
-    const MENU_BUTTON = document.querySelector(".fa-bars");
-    const SEARCH_BUTTON = document.querySelector(".fa-search");
-    const SELECT_TITLE = document.querySelector('.main-content-todolist-select')
-
-
-
-    const DRAGGABLE_ZONE = document.querySelector('ul');
-
-
-
-
-    SEARCH_INPUT.addEventListener('input', () => {
+    INPUT_SEARCH_TEXT.addEventListener('input', () => {
         searchHandler();
-    }
-    );
+    });
 
-    ADD_FORM.addEventListener("submit", (event) => {
+    FORM_NEW_TODO.addEventListener("submit", (event) => {
         event.preventDefault();
         createTaskHandler(event);
     });
@@ -44,20 +35,20 @@ export function addEventsListenerHandler() {
 
     MENU_BUTTON.addEventListener("click",
         () => {
-            toogleMainHeaderDisplay()
+            displayToogle(MAIN_HEADER_MENU);
         });
 
     SEARCH_BUTTON.addEventListener("click", () => {
-        toogleSearchBarDisplay();
+        displayToogle(FORM_SEARCH);
     });
 
 
     ADD_FORM_BUTTON.addEventListener("click", function (event) {
         event.preventDefault();
-        toogleAddForm();
+        displayToogle(FORM_NEW_TODO);
     });
 
-    SELECT_TITLE.addEventListener('change', () => {
+    SELECT_CURRENT_LIST.addEventListener('change', () => {
         removeAllTasks();
         const array = getTasksArray(SELECT_TITLE.value);
         array.forEach(element => {
@@ -67,7 +58,7 @@ export function addEventsListenerHandler() {
 
     LANDING_FORM.addEventListener('submit', (event) => {
         event.preventDefault();
-        hideLandingPopUp();
+        displayToogle(POPUP_LANDING);
         const key = getPopUpLandingInput();
         setTasksArray(key);
         addOptiontoSelect(key);
@@ -80,29 +71,4 @@ export function addEventsListenerHandler() {
     DRAGGABLE_ZONE.addEventListener('dragover', (event) => {
         swapHandler(DRAGGABLE_ZONE, event);
     })
-
-
 }
-
-    // LANDING_PAGE.addEventListener('click', instructionsHandler);
-
- // const picsArray = ["'/assets/styles/resources/LandingPage/Tips.jpg'",
-    //     "'/assets/styles/resources/LandingPage/addTodo.jpg'",
-    //     "'/assets/styles/resources/LandingPage/search.jpg'",
-    //     "'/assets/styles/resources/LandingPage/openMenu.jpg'",
-    //     "'/assets/styles/resources/LandingPage/createList.jpg'"
-    // ]
-    // let indexPicsArray = 0;
-
-    // function instructionsHandler() {
-    //     if (indexPicsArray < 4) {
-    //         LANDING_PAGE.style.backgroundImage = `url(${picsArray[indexPicsArray++]})`;
-    //     }
-    //     else if (indexPicsArray === 4) {
-    //         LANDING_PAGE.style.backgroundImage = `url(${picsArray[indexPicsArray++]})`;
-    //         displayLandingPopUPForm();
-    //     }
-    //     else {
-    //         LANDING_PAGE.removeEventListener('click', instructionsHandler)
-    //     }
-    // }
